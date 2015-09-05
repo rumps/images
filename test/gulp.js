d600deda4c3ce55209c52cabe083a07161600159
jupl <juan.placencia.512@gmail.com>
2015-09-05 15:46:03 -0500

    Update info task test

--- test/gulp.js
+++ test/gulp.js
@@ -82,7 +82,7 @@ describe('tasks', function describeTasks() {
     console.log = newLog
     gulp.start('spec:info')
     console.log = log
-    logs.length.should.not.be.above(4)
+    logs.length.should.not.be.above(5)
 
     function newLog(...args) {
       logs.push(stripColor(args.join(' ')))
